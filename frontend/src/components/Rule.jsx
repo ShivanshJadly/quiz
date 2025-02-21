c471e21f92e2dbfcd2a330826fae552daab8ba39
Siddharth <siddharthtomar003@gmail.com>
2025-02-21 17:14:48 +0530

    minor changes

--- frontend/src/components/Rule.jsx
+++ frontend/src/components/Rule.jsx
@@ -3,9 +3,9 @@ import heading from './heading'
 
 const Rule = () => {
   return (
-    <div className='relative flex justify-end mt-'>
+    <div className='relative flex justify-end p-2'>
       <section className='absolute text-center text-8xl text-[#FFC107] font-bold bottom-0 right-[42%]'><h1>Quiz!!</h1></section>
-      <section className="border-4 border-white rounded-md p-4 w-[20%] bg-[#FFC107] font-bold">
+      <section className="border-4 border-black rounded-md p-4 w-[20%] bg-[#FFC107] font-bold">
         <ul>
         <h1 className='underline text-lg mb-2'>Rules:</h1>
           <li>1. You will be given 5 questions</li>
