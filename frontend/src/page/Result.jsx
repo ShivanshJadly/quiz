b967eed29410011494e24f2ff1f63019e1c5d43c
ShivanshJadly <shivanshjadly0@gmail.com>
2025-02-21 23:29:25 +0530

    mc

--- frontend/src/page/Result.jsx
+++ frontend/src/page/Result.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import Heading from "../components/heading";
 import { getUser } from "../services/operations/authAPI";
 import { selectCurrentUser } from "../slices/authSlice";
 
